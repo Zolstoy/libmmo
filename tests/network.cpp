c47e5e2d961d354aaf1d50f80bffc3efc098ba3f
Zolstoy <zolstoy.dev@gmail.com>
2025-06-05 22:17:28 +0200

    wip

--- tests/network.cpp
+++ tests/network.cpp
@@ -11,17 +11,14 @@
 #include <mmo/protocol.hpp>
 
 #include <cereal/archives/json.hpp>
-#include <fmt/color.h>
 #include <gtest/gtest.h>
 #include <spdlog/common.h>
 #include <spdlog/pattern_formatter.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #include "mmo/event.hpp"
 
 #include "common.hpp"
-
 
 using namespace boost;
 
