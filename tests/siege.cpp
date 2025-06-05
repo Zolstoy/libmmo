6a8c0afda882ba62bf7a2aea7841350d0b6f03e1
Zolstoy <zolstoy.dev@gmail.com>
2025-06-05 22:13:12 +0200

    wip

--- tests/siege.cpp
+++ tests/siege.cpp
@@ -10,6 +10,5 @@ using namespace boost;
 
 TEST(siege, scenario_01_normal)
 {
-    asio::io_context ioc;
-    ASSERT_NO_THROW(mmo::instance(ioc, get_random_instance_path(), 0, CERT, KEY, [](mmo::event &&) { return true; }));
+    ASSERT_NO_THROW(mmo::instance(get_random_instance_path(), 0, CERT, KEY, [](mmo::event &&) { return true; }));
 }
