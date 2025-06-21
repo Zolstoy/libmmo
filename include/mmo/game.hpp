e395035f146511d5084779849c9abe4b9de96de2
Zolstoy <zolstoy.dev@gmail.com>
2025-06-21 17:26:05 +0200

    BIGWIP

--- include/mmo/game.hpp
+++ include/mmo/game.hpp
@@ -1,12 +1,172 @@
 #pragma once
 
+#include "config.hpp"
+
+#include <concepts>
+#include <cstddef>
+#include <string>
+#include <thread>
+
+#include "persistence/sqlite_database.hpp"
+#include "transport/secure_websocket.hpp"
+
 namespace mmo {
 
-template <typename T>
-concept Game = requires {
+template <class T>
+concept Persistence = requires(T t, std::string const &s, float f) {
     {
-        T::Player
-    };
+        t.save_user(s, s)
+    } -> std::same_as<std::expected<std::tuple<>, error>>;
+    {
+        t.load_user_player(s, s)
+    } -> std::same_as<std::expected<player, error>>;
+    {
+        t.set_init_position(f, f)
+    } -> std::same_as<void>;
+};
+
+template <class T>
+concept Transport = requires(T t) {
+    {
+        t.start()
+    } -> std::same_as<void>;
+    {
+        t.stop()
+    } -> std::same_as<void>;
+};
+
+template <class T>
+concept Registrar = requires(T t, std::string const &s) {
+    {
+        t.register_user(s, s)
+    } -> std::same_as<std::expected<std::tuple<>, error>>;
+    {
+        t.authenticate_user(s, s)
+    } -> std::same_as<std::expected<size_t, error>>;
+};
+
+template <class T>
+concept Scheduler = requires(T t) {
+    {
+        t.launch()
+    } -> std::same_as<void>;
+};
+
+template <class T>
+concept Cycle = requires(T t) {
+    {
+        t.cycle()
+    } -> std::same_as<void>;
+};
+
+template <class T>
+concept Authenticator = requires(T t, std::string const &s) {
+    {
+        t.authenticate_user(s, s)
+    } -> std::same_as<std::expected<size_t, error>>;
+};
+
+template <class T>
+concept Implementation = Persistence<T> && Transport<T> && Registrar<T> && Authenticator<T> && Cycle<T> && Scheduler<T>;
+
+class mono_threaded
+{
+   public:
+    void launch()
+    {}
+};
+
+class secure_websocket
+{
+   public:
+    void start()
+    {}
+
+    void stop()
+    {}
+};
+
+class email_confirm
+{
+   public:
+    email_confirm(std::string const &smtp_username, std::string const &smtp_password, std::string const &smtp_url)
+    {}
+
+   public:
+    std::expected<std::tuple<>, error> register_user(std::string const &nickname, std::string const &password)
+    {
+        return std::tuple{};
+    }
+
+    // std::expected<size_t, error> authenticate_user(std::string const &nickname, std::string const &password)
+    // {
+    //     return std::make_expected<size_t, error>(0);
+    // }
+};
+
+template <Persistence P>
+class nickname_password
+{
+   private:
+    P &persistence_;
+
+   public:
+    nickname_password(P &persistence)
+        : persistence_(persistence)
+    {}
+
+   public:
+    std::expected<size_t, error> authenticate_user(std::string const &nickname, std::string const &password)
+    {
+        return 0;
+    }
+};
+
+template <Cycle C, Registrar R, Authenticator A, Persistence P, Transport T, Scheduler S>
+class MMO_API game
+{
+   private:
+    C &cycle_;
+    R &registrar_;
+    A &authenticator_;
+    P &persistence_;
+    T &transport_;
+    S &scheduler_;
+
+   public:
+    template <Implementation I>
+    game(I &implem)
+        : game(implem, implem, implem, implem, implem, implem)
+    {}
+
+    game(C &cycle, R &registrar, A &authenticator, P &persistence, T &transport, S &scheduler)
+        : cycle_(cycle)
+        , registrar_(registrar)
+        , authenticator_(authenticator)
+        , persistence_(persistence)
+        , transport_(transport)
+        , scheduler_(scheduler)
+    {}
+
+    game(C &cycle, std::string const &world_name, std::string const &cert_pem_path, std::string const &key_pem_path,
+         std::string const &smtp_username, std::string const &smtp_password, std::string const &smtp_url)
+        : cycle_(cycle
+        , registrar_(email_confirm{smtp_username, smtp_password, smtp_url})
+        , authenticator_(nickname_password<sqlite_database>{persistence_})
+        , persistence_(sqlite_database{world_name})
+        , transport_(secure_websocket{cert_pem_path, key_pem_path, smtp_username, smtp_password, smtp_url})
+        , scheduler_(mono_threaded{})
+    {
+        persistence_.set_init_position(0.0f, 0.0f);
+    }
+
+    template <class Rep, class Period = std::ratio<1>>
+    void run(std::chrono::duration<Rep, Period> const &tick_duration)
+    {
+        transport_.start();
+        std::this_thread::sleep_for(tick_duration);
+        transport_.stop();
+    }
 };
 
 }   // namespace mmo
---
+++ tests/game.cpp
@@ -0,0 +1,10 @@
+#include <mmo/game.hpp>
+
+#include <gtest/gtest.h>
+
+// #include "common.hpp"
+
+TEST(game, all)
+{
+    // ASSERT_NO_THROW(mmo::sqlite_database db(get_random_instance_path()));
+}
